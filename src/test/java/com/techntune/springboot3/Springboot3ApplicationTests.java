4b80e143960d2f96ea0fd848a72d16fd8d9755cc
s4um1l <saumildade@gmail.com>
2022-12-04 17:22:17 -0600

    initial commit spring boot3

---
+++ src/test/java/com/techntune/springboot3/Springboot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techntune.springboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
